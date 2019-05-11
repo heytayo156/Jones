289f5565208a99c0904b39b01cea5ca80820c0d5
JafarArif <50292888+heytayo156@users.noreply.github.com>
2019-05-11 21:30:34 +0700

    Add files via upload

---
+++ Squ4r3-B4D.sh
@@ -0,0 +1,52 @@
+clear
+echo
+echo "|------------------------------------------|" | lolcat
+echo "|               Squ4r3-B4D                 |" | lolcat
+echo "|                434ERROR                  |" | lolcat
+echo "|                _Faedah_                  |" | lolcat
+echo "|------------------------------------------|" | lolcat
+
+echo -n "Masukan Nama Loe : " | lolcat
+read Loe
+echo -n "Contoh Gaya *contoh : Tiduran * : " | lolcat
+read Gaya
+echo -n "Nama Pacar Yang akan DiHamilin : " | lolcat
+read Nama
+echo -n "Alamat Untuk Ngeue : " | lolcat
+read Alamat
+echo -n "Mau Masukin Kontol dari atas/bawah : " | lolcat
+read Masukin
+echo -n "Mau Punya Anak Berapa : " | lolcat
+read Anak
+echo " Dosa Tanggung Sendiri / Suami Istri Okeee " | lolcat
+sleep 3
+clear
+echo "Lagi Lepas Baju Coook" | lolcat
+echo "Bang Mulai Ayoooh Ah Ahhhh Ahhhhh" | lolcat
+echo "Mpshhh Ahhhh Ahhh" | lolcat
+echo "Masih Kuat Gak Yaank?" | lolcat
+echo "Masih Baang , Lanjuuuut!" | lolcat
+echo "Sebentar Lagi Keluar Pejuh Yaank" | lolcat
+echo "Ikeeh Ikeeeh" | lolcat
+echo "Bang aku dah Nggak Kuat Lagi :(" | lolcat
+echo "Sama Yank Ya udah berhenti Yaaaa :v" | lolcat 
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 2
+echo "|=======================================|" | lolcat
+echo "|          Cewe Loe Dah Hamil           |" | lolcat
+echo "|=================343===================|" | lolcat
+echo 
+echo
+echo "Berhasil Sayank Kamu Dah Hamil!!,Anda telah Punya Anak $Anak . $Loe , Si $Nama  Minta di Hamilin Lagi di $Alamat!" | lolcat
+echo "pake Gaya $Gaya Katanya!" | lolcat
+echo
+echo
+echo "Dosa Tanggung Sendiri Coeeg!" | lolcat
+echo "Gwe Kagak Tanggung Jawab AJG" | lolcat
+
